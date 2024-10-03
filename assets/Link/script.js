f010faf1285a763a3717a83c24b5a473b7445b17
Nshizirungu <nshizirungushizon@gmail.com>
2024-10-03 17:26:00 +0300

    This is portiflio

---
+++ assets/Link/script.js
@@ -0,0 +1,14 @@
+function openProject(){
+    document.getElementById("project").style.display = "block"
+}
+function closeProject(){
+    document.getElementById("project").style.display = "none"
+
+}
+function openContact(){
+    document.getElementById("contact").style.display = "block"
+}
+function closeContact(){
+    document.getElementById("contact").style.display = "none"
+
+}
